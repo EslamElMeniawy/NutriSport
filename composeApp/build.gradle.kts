b490003371c62e9a187dea580e5e883c0797f9c4
Eslam El-Meniawy <eelmeniawy@ejada.com>
2025-08-07 15:40:26 +0300

    feat: add `Android` splash screen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.splash.screen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
