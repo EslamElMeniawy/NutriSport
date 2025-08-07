f2cd43deae6514500b57541a2104f27f09edde48
Eslam El-Meniawy <eelmeniawy@ejada.com>
2025-08-07 16:49:22 +0300

    feat: add `Fonts`, `Colors` & `Resources`

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
@@ -15,18 +15,18 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeApp"
+            baseName = "shared"
             isStatic = true
         }
     }
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
@@ -50,32 +50,17 @@ kotlin {
 }
 
 android {
-    namespace = "elmeniawy.eslam.nutrisport"
+    namespace = "elmeniawy.eslam.nutrisport.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "elmeniawy.eslam.nutrisport"
         minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
-
-dependencies {
-    debugImplementation(compose.uiTooling)
+    testOptions {
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
 }
---
+++ shared/src/commonMain/kotlin/elmeniawy/eslam/nutrisport/shared/Fonts.kt
@@ -0,0 +1,36 @@
+package elmeniawy.eslam.nutrisport.shared
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+import nutrisport.shared.generated.resources.Res
+import nutrisport.shared.generated.resources.bebas_neue_regular
+import nutrisport.shared.generated.resources.roboto_condensed_medium
+import org.jetbrains.compose.resources.Font
+
+/**
+ * Fonts
+ *
+ * Created by Eslam El-Meniawy on 07-Aug-2025 at 4:18 PM.
+ */
+
+@Composable
+fun BebasNeueFont() = FontFamily(
+    Font(Res.font.bebas_neue_regular)
+)
+
+@Composable
+fun RobotoCondensedFont() = FontFamily(
+    Font(Res.font.roboto_condensed_medium)
+)
+
+object FontSize {
+    val EXTRA_SMALL = 10.sp
+    val SMALL = 12.sp
+    val REGULAR = 14.sp
+    val EXTRA_REGULAR = 16.sp
+    val MEDIUM = 18.sp
+    val EXTRA_MEDIUM = 20.sp
+    val LARGE = 30.sp
+    val EXTRA_LARGE = 40.sp
+}
